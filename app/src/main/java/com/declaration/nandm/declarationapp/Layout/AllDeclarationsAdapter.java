8facc98f1aee525453e13f719b3bda27ed9ac81a
mauro <i390383@fhict.nl>
2018-10-26 12:26:48 +0200

    Uitklappen werkt nu aleen nog mooi maken

--- app/src/main/java/com/declaration/nandm/declarationapp/Layout/AllDeclarationsAdapter.java
+++ app/src/main/java/com/declaration/nandm/declarationapp/Layout/AllDeclarationsAdapter.java
@@ -2,9 +2,11 @@ package com.declaration.nandm.declarationapp.Layout;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
 import android.transition.TransitionManager;
 import android.util.DisplayMetrics;
+import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,12 +18,21 @@ import com.declaration.nandm.declarationapp.R;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AllDeclarationsAdapter extends RecyclerView.Adapter<MyViewHolder> {
+public class AllDeclarationsAdapter extends RecyclerView.Adapter<DeclarationViewHolder> {
 
 
     private List<Declaration> declarations;
     private Context mContext;
+    private SparseBooleanArray expandState = new SparseBooleanArray();
     private int mExpandedPosition = -1;
+
+    public AllDeclarationsAdapter(List<Declaration> declarations) {
+        this.declarations = declarations;
+        //set initial expanded state to false
+        for (int i = 0; i < declarations.size(); i++) {
+            expandState.append(i, false);
+        }
+    }
 
     public AllDeclarationsAdapter(Context mContext) {
         declarations = new ArrayList<>();
@@ -35,15 +46,15 @@ public class AllDeclarationsAdapter extends RecyclerView.Adapter<MyViewHolder> {
 
     @NonNull
     @Override
-    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+    public DeclarationViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
         View itemView = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.declarations_cardview, parent, false);
 
-        return new MyViewHolder(itemView);
+        return new DeclarationViewHolder(itemView);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final DeclarationViewHolder holder, final int position) {
 
         Declaration declaration = declarations.get(position);
         holder.title.setText(declaration.getAuthority());
@@ -63,11 +74,64 @@ public class AllDeclarationsAdapter extends RecyclerView.Adapter<MyViewHolder> {
             default:
                 holder.state.setImageResource(R.color.statusPending);
         }
+
+
+        holder.title2.setText(declaration.getAuthority());
+        holder.description2.setText(declaration.getDescription());
+        holder.price2.setText("€" + String.valueOf(declaration.getPrice()));
+
+        switch(declaration.getState()){
+            case Declined:
+                holder.state2.setImageResource(R.color.statusDeclined);
+                break;
+            case Accepted:
+                holder.state2.setImageResource(R.color.statusAccepted);
+                break;
+            case Pending:
+                holder.state2.setImageResource(R.color.statusPending);
+                break;
+            default:
+                holder.state2.setImageResource(R.color.statusPending);
+        }
+
+
+        final boolean isExpanded = expandState.get(position);
+        holder.expandableLayout.setVisibility(isExpanded?View.VISIBLE:View.GONE);
+        holder.originalLayout.setVisibility(isExpanded?View.VISIBLE:View.VISIBLE);
+
+       // holder.stateClick.setRotation(expandState.get(position) ? 180f : 0f);
+        holder.originalLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                onClickButton(holder.expandableLayout,holder.originalLayout ,position);
+            }
+        });
+        holder.expandableLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                onClickButton(holder.expandableLayout,holder.originalLayout ,position);
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
         return declarations.size();
+    }
+
+    private void onClickButton(final ConstraintLayout expandableLayout, final ConstraintLayout originalLayout,final  int i) {
+
+        //Simply set View to Gone if not expanded
+        //Not necessary but I put simple rotation on button layout
+        if (expandableLayout.getVisibility() == View.VISIBLE){
+            originalLayout.setVisibility(View.VISIBLE);
+            expandableLayout.setVisibility(View.GONE);
+            expandState.put(i, false);
+        }else{
+            originalLayout.setVisibility(View.GONE);
+            expandableLayout.setVisibility(View.VISIBLE);
+            expandState.put(i, true);
+        }
     }
 }
 
--- app/src/main/java/com/declaration/nandm/declarationapp/Layout/DeclarationViewHolder.java
+++ app/src/main/java/com/declaration/nandm/declarationapp/Layout/DeclarationViewHolder.java
@@ -2,28 +2,40 @@ package com.declaration.nandm.declarationapp.Layout;
 
 
 import android.content.Context;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.declaration.nandm.declarationapp.R;
 
 
-public class MyViewHolder extends RecyclerView.ViewHolder {
+public class DeclarationViewHolder extends RecyclerView.ViewHolder {
 
     public TextView title, description, price;
-    public ImageView state;
+    public TextView title2, description2, price2;
 
+    public ImageView state, state2;
+    public ConstraintLayout originalLayout, expandableLayout;
 
-    public MyViewHolder(View view) {
+    public DeclarationViewHolder(View view) {
         super(view);
         title = (TextView) view.findViewById(R.id.title);
         description = (TextView) view.findViewById(R.id.description);
         state = (ImageView) view.findViewById(R.id.overflow);
         price = (TextView) view.findViewById(R.id.price);
+
+        title2 = (TextView) view.findViewById(R.id.title2);
+        description2 = (TextView) view.findViewById(R.id.description2);
+        state2 = (ImageView) view.findViewById(R.id.overflow2);
+        price2 = (TextView) view.findViewById(R.id.price2);
+
+        expandableLayout = (ConstraintLayout) view.findViewById((R.id.expandableLayout));
+        originalLayout = (ConstraintLayout) view.findViewById((R.id.originalLayout));
     }
 
 
